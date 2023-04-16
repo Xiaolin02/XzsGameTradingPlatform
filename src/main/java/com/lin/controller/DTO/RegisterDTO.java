10767956e73a9341f2fd1229548e74864d7cc87b
XiaoLin <2013258745@qq.com>
2023-04-16 21:48:13 +0800

    v1.0

---
+++ src/main/java/com/lin/controller/DTO/RegisterDTO.java
@@ -0,0 +1,17 @@
+package com.lin.controller.DTO;
+
+import lombok.Data;
+
+/**
+ * @author 林炳昌
+ * @desc 注册模型
+ * @date 2023年04月16日 19:20
+ */
+@Data
+public class RegisterDTO {
+
+    private String phone;
+
+    private String code;
+
+}

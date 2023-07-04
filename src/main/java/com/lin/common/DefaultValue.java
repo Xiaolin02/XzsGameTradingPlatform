3ed831cd82d6829f99834c4daa6920f4dc9c45d7
emoration <3388356471@qq.com>
2023-07-04 09:57:06 +0800

    fixed:修复了商品的默认封面问题

---
+++ src/main/java/com/lin/common/DefaultValue.java
@@ -0,0 +1,17 @@
+package com.lin.common;
+
+/**
+ * @Author czh
+ * @desc 默认值
+ * @date 2023/7/4 9:46
+ */
+public interface DefaultValue {
+    /**
+     * 默认封面url
+     */
+    String DEFAULT_COVER_URL = "https://github.githubassets.com/images/modules/site/home/globe.jpg?width=742";
+    /**
+     * 默认用户头像
+     */
+    String DEFAULT_USER_AVATAR_URL = "https://github.githubassets.com/images/modules/site/eyebrow-banner-icon-copilot-x.svg";
+}
